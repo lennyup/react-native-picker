82b94eb45133f0b6aeb521a6c5e98b51c8be480e
lennyup <jsonlenny@163.com>
2016-08-26 11:31:04 +0800

    Update index.js
    add prickerCanleBtnStyle, makeColor

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ export default class PickerAny extends Component {
 		pickerBtnStyle: Text.propTypes.style,
 		pickerTitle: PropTypes.string,
 		pickerTitleStyle: Text.propTypes.style,
+		pickerCancelBtnStyle: Text.propTypes.style,
 		pickerToolBarStyle: View.propTypes.style,
 		pickerBoxStyle: View.propTypes.style,
 		showMask: PropTypes.bool,
@@ -420,7 +421,7 @@ export default class PickerAny extends Component {
 				<View style={[styles.pickerBox, this.state.style, this.state.pickerBoxStyle]}>
 					<View style={[styles.pickerToolbar, this.state.pickerToolBarStyle, {width: this.state.style.width || width}]}>
 						<View style={styles.pickerCancelBtn}>
-							<Text style={[styles.pickerFinishBtnText, this.state.pickerBtnStyle]}
+							<Text style={[styles.pickerFinishBtnText, this.state.pickerBtnStyle, this.state.pickerCancelBtnStyle]}
 								onPress={this._pickerCancel.bind(this)}>{this.state.pickerCancelBtnText}</Text>
 						</View>
 						<Text style={[styles.pickerTitle, this.state.pickerTitleStyle]} numberOfLines={1}>
@@ -456,7 +457,7 @@ let styles = StyleSheet.create({
 	mask: {
 		position: 'absolute',
 		top: 0,
-		backgroundColor: 'transparent',
+		backgroundColor: 'rgba(0,0,0,0)',
 		height: height,
 		width: width
 	},
